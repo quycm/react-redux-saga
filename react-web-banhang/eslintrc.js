b2a3b7c71534b856fbd662fc64ce2298941aab1c
quy chu <quycm2802@gmail.com>
2020-10-06 23:12:13 +0700

    quy commit code

--- react-web-banhang/eslintrc.js
+++ react-web-banhang/eslintrc.js
@@ -1,37 +1,43 @@
 module.exports = {
-    parser: "babel-eslint",
-    parserOptions: {
-        ecmaVersion: 6,
-        sourceType: "module",
-        ecmaFeatures: {
-            jsx: true
-        }
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    rules: {
-        semi: 1,
-        quotes: [2, 'single'],
-        'react/prop-types': 1,
-        'react/jsx-max-props-per-line': 1,
-        'linebreak-style' : 0,
-        'import/no-extraneous-dependencies': 0,
-        'class-methods-use-this': 0,
-        'react/jsx-filename-extension': 0,
-        'react/jsx-one-expression-per-line':0,
-        'react/forbid-prop-types': 0,
-        'react/require-default-props': 0,
-        'prettier/prettier' : ['error'],
-        'no-unuserd-vars': 1,
-        'import/extensions': 0,
-        'import/order': 1,
-        'react/prefer-stateless-function': 0,
-        'no-use-before-define': 0,
-        'no-param-reassign': 0,
-        'import/no-useless-path-segements':0,
-        'import/prefer-default-export': 0,
-        'react/jsx-uses-vars': 2
-    },
-    plugins: ['prettier'],
-    env : {
-        "es6": true
-    }
-}
+  },
+  rules: {
+    semi: 1,
+    quotes: [2, 'single'],
+    'react/prop-types': 1,
+    'react/jsx-max-props-per-line': 1,
+    'linebreak-style': 0,
+    'import/no-extraneous-dependencies': 0,
+    'class-methods-use-this': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/forbid-prop-types': 0,
+    'react/require-default-props': 0,
+    'no-unuserd-vars': 1,
+    'import/extensions': 0,
+    'import/order': 1,
+    'react/prefer-stateless-function': 0,
+    'no-use-before-define': 0,
+    'no-param-reassign': 0,
+    'import/no-useless-path-segements': 0,
+    'import/prefer-default-export': 0,
+    'react/jsx-uses-vars': 2,
+    'react/jsx-closing-bracket-location': 1,
+    'react/jsx-first-prop-new-line': 1,
+    'prettier/prettier': ['error', {
+        endOfLine: 'auto'
+    }],
+  },
+  plugins: ['prettier'],
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+};
